95a2d37419f8ecd8188189e952436ffce795d30e
Afiq Haekal <theexorcist78@gmail.com>
2026-03-10 00:39:57 +0700

    fix: add ShimmerLoading widget class

--- lib/widgets/shimmer_loading.dart
+++ lib/widgets/shimmer_loading.dart
@@ -108,17 +108,79 @@ class ShimmerGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
-        childAspectRatio: childAspectRatio,
-        crossAxisSpacing: 10,
         mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        childAspectRatio: childAspectRatio,
       ),
       itemCount: itemCount,
-      itemBuilder: (_, __) => const ShimmerCard(width: double.infinity, height: 150),
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, _) => const ShimmerCard(),
+    );
+  }
+}
+
+/// Generic shimmer placeholder with configurable size and border radius.
+class ShimmerLoading extends StatefulWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
+
+  const ShimmerLoading({
+    super.key,
+    required this.width,
+    required this.height,
+    this.borderRadius = 0,
+  });
+
+  @override
+  State<ShimmerLoading> createState() => _ShimmerLoadingState();
+}
+
+class _ShimmerLoadingState extends State<ShimmerLoading>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Container(
+          width: widget.width,
+          height: widget.height,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            gradient: LinearGradient(
+              begin: Alignment(-1.0 + 2.0 * _controller.value, 0),
+              end: Alignment(-1.0 + 2.0 * _controller.value + 1, 0),
+              colors: const [
+                Color(0xFF1E1E3A),
+                Color(0xFF2A2A4A),
+                Color(0xFF1E1E3A),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
